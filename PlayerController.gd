6a0c86dadcdae66015ff4fff24516d0aa3228592
Srayan Jana <valorzard.kiko@outlook.com>
2022-05-15 10:20:28 -0700

    change names

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-const WALK_FORCE = 600
-const WALK_MAX_SPEED = 600
+const MOVE_FORCE = 600
+const GROUND_MAX_SPEED = 400
 const STOP_FORCE = 1300
 const JUMP_SPEED = 200
 
@@ -11,15 +11,15 @@ onready var gravity = 200
 
 func _physics_process(delta):
 	# Horizontal movement code. First, get the player's input.
-	var walk = WALK_FORCE * (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
+	var walk = MOVE_FORCE * (Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
 	# Slow down the player if they're not trying to move.
-	if abs(walk) < WALK_FORCE * 0.2:
+	if abs(walk) < MOVE_FORCE * 0.2:
 		# The velocity, slowed down a bit, and then reassigned.
 		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
 	else:
 		velocity.x += walk * delta
 	# Clamp to the maximum horizontal movement speed.
-	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
+	velocity.x = clamp(velocity.x, -GROUND_MAX_SPEED, GROUND_MAX_SPEED)
 
 	# Vertical movement code. Apply gravity.
 	velocity.y += gravity * delta
