18c46cca4d7ce688a73626d7243a6a4c44811c2d
Srayan Jana <valorzard.kiko@outlook.com>
2022-05-24 10:08:35 -0700

    meh, combo stuff is better, but still needs more work.
    i need to refactor the hell out of all of this

---
+++ actors/enemy/EnemyController.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+class_name Enemy
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var combo_hit_counter : int = 0
+var current_stun : int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# make sure that this is part of Enemy group
+	add_to_group("enemies")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if !is_on_floor():
+		move_and_slide(Vector2(0, 100))
+	# deal with stun/combos; drop combo if no long in stun
+	if current_stun > 0:
+		current_stun -= 1
+		# change color to show if in hitstun
+		self.modulate = Color(0, 0, 1)
+		# put enemy hit into attacked enemy list
+		if not self in AttackManager.list_of_enemies_in_stun:
+			AttackManager.list_of_enemies_in_stun.push_back(self)
+			#print("push!")
+	else:
+		combo_hit_counter = 0
+		self.modulate = Color(1, 1, 1)
+		if self in AttackManager.list_of_enemies_in_stun:
+			AttackManager.list_of_enemies_in_stun.erase(self)
+			#print("pog!")
+
+func add_combo_hit(hit : int, stun : int, pushback : Vector2):
+	combo_hit_counter += hit
+	current_stun = stun
+	move_and_slide(pushback)
---
+++ singletons/AttackManager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var combo_count : int = 0
+var list_of_enemies_in_stun : Array
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
