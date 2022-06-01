7f5c874ed618b50c386761b9813e0fe5d3417c75
Srayan Jana <valorzard.kiko@outlook.com>
2022-06-01 13:14:19 -0700

    combo counter is currently broken

--- actors/enemy/EnemyController.gd
+++ actors/enemy/EnemyController.gd
@@ -6,11 +6,11 @@ class_name Enemy
 # var a = 2
 # var b = "text"
 
-var combo_hit_counter : int = 0
+#var combo_hit_counter : int = 0
 var current_stun : int = 0
 var current_pushback : Vector2
 var current_pushback_time : int = 0
-var health : int = 10
+export var health : int = 10
 
 # check if enemy got hit so that we can add to the combo counter enemy list
 var is_hit : bool = false
@@ -19,7 +19,9 @@ var is_hit : bool = false
 func _ready():
 	# make sure that this is part of Enemy group
 	add_to_group("enemies")
-	pass # Replace with function body.
+	# set up progress bar
+	$ProgressBar.max_value = health
+	$ProgressBar.value = health
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,11 +33,8 @@ func _physics_process(delta):
 		current_stun -= 1
 		# change color to show if in hitstun
 		self.modulate = Color(0, 0, 1)
-		
-		if not AttackManager.list_of_enemies_in_stun.has(self):
-			AttackManager.list_of_enemies_in_stun.push_back(self)
 	else:
-		combo_hit_counter = 0
+		#combo_hit_counter = 0
 		self.modulate = Color(1, 1, 1)
 		is_hit = false
 		if AttackManager.list_of_enemies_in_stun.has(self):
@@ -46,13 +45,26 @@ func _physics_process(delta):
 		move_and_slide(current_pushback)
 		
 
-func add_combo_hit(hit : int, stun : int, pushback : Vector2, pushback_time: int):
-	combo_hit_counter += hit
+func add_combo_hit(hit : int, damage : int, stun : int, pushback : Vector2, pushback_time: int):
+	if len(AttackManager.list_of_enemies_in_stun) == 0:
+		# reset the combo count back to one if enemy is no longer in stun
+		AttackManager.combo_count = 1
+		# put it into the stun queue
+		AttackManager.list_of_enemies_in_stun.push_back(self)
+	else:
+		AttackManager.combo_count += 1
+	
+	#combo_hit_counter += hit
 	current_stun = stun
 	current_pushback = pushback
 	current_pushback_time = pushback_time
-	health -= hit
-	get_node("ProgressBar").value -= hit
+	health -= damage
+	get_node("ProgressBar").value -= damage
 	if health <= 0:
-		get_parent().remove_child(self)
-	
+		on_death()
+
+func on_death():
+	# erase self from list CURRENTLY BUGGED
+	if AttackManager.list_of_enemies_in_stun.has(self):
+		AttackManager.list_of_enemies_in_stun.erase(self)
+	self.queue_free()
